5f47e07a39d7f08ab7f03f3f27355bdc4da836cb
radu <radustanciu841@yahoo.ro>
2021-09-16 14:26:55 +0300

    Change width buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 }
