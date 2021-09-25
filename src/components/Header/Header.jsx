edeae6aa3c37dcde0d2b221bfb0631083322f8d0
radu <radustanciu841@yahoo.ro>
2021-09-25 13:03:33 +0300

    Add the Link component from react-router-dom

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import "../../components/Header/Header.css";
 import logo from "../../assets/images/logo.png";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
       <img src={logo} alt="Logo" />
-        <input type="search" placeholder="Cauta" />
+      <input type="search" placeholder="Cauta" />
       <div className="header-buttons">
-        <button type="button" className="btn btn-primary">
-          Log In
-        </button>
-        <button type="button" className="sign-up">
-          Sign Up
-        </button>
+        <Link to='/login'>
+          <button type="button" className="btn btn-primary">
+            Log In
+          </button>
+        </Link>
+        <Link to='/sign_up'>
+          <button type="button" className="sign-up">
+            Sign Up
+          </button>
+        </Link>
       </div>
     </div>
   );
