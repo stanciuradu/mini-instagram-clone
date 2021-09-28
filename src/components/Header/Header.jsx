bac109216c3f85b2f1fc8048aedc2918fc018c1f
radu <radustanciu841@yahoo.ro>
2021-09-28 21:36:51 +0300

    Modify styling Header from CSS in SCSS

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../../components/Header/Header.css";
+import '../Header/Header.scss';
 import logo from "../../assets/images/logo.png";
 import { Link } from "react-router-dom";
 
